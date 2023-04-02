0c5e7306e20113f4927dad7585be8372d0263397
Mohammed Misbahuddin <misbahuddinsaudagar@gmail.com>
2023-04-02 11:35:44 +0530

    V.8.0

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify
 # import pickle
 import numpy as np
 import pandas as pd
-import numpy as np
-# import random
+#import numpy as np
+import random
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -26,11 +26,40 @@ from sklearn.model_selection import train_test_split
 # New_predict = logit.predict(X_predict)
 # print(New_predict)
 
+# Reading data from csv file
+data = pd.read_csv('urldata.csv')
+data.head()
+
+
+# Labels
+y = data["label"]
+
+# Features
+url_list = data["url"]
+
+
+# Using Tokenizer
+vectorizer = TfidfVectorizer()
+
+# Store vectors into X variable as Our XFeatures
+X = vectorizer.fit_transform(url_list)
+
+
+
+# Split into training and testing dataset 80:20 ratio
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Model Building using logistic regression
+logit = LogisticRegression()
+logit.fit(X_train, y_train)
+
+
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return "hello world, this is a test"
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -38,34 +67,6 @@ def predict():
     #result={'url':url}
     input_query=np.array([url])
     #print(input_query)
-
-    # Reading data from csv file
-    data = pd.read_csv('urldata.csv')
-    data.head()
-
-
-    # Labels
-    y = data["label"]
-
-    # Features
-    url_list = data["url"]
-
-
-    # Using Tokenizer
-    vectorizer = TfidfVectorizer()
-
-    # Store vectors into X variable as Our XFeatures
-    X = vectorizer.fit_transform(url_list)
-
-
-
-    # Split into training and testing dataset 80:20 ratio
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-    # Model Building using logistic regression
-    logit = LogisticRegression()
-    logit.fit(X_train, y_train)
 
     #vectorizer = TfidfVectorizer()
     input_query = vectorizer.transform(input_query)
