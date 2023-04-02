f2e8b270ed8312d992919eee67bd4c68601b2260
Mohammed Misbahuddin <misbahuddinsaudagar@gmail.com>
2023-04-02 14:58:01 +0530

    V.11

--- app.py
+++ app.py
@@ -10,6 +10,32 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
+# Reading data from csv file
+data = pd.read_csv('urldata.csv')
+data.head()
+
+# Labels
+y = data["label"]
+
+# Features
+url_list = data["url"]
+
+
+# Using Tokenizer
+vectorizer = TfidfVectorizer()
+
+# Store vectors into X variable as Our XFeatures
+X = vectorizer.fit_transform(url_list)
+
+
+
+# Split into training and testing dataset 80:20 ratio
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Model Building using logistic regression
+logit = LogisticRegression()
+logit.fit(X_train, y_train)
 
 # url="google.com/search=faizanahmad"
 # input_query=np.array([url])
@@ -33,42 +59,11 @@ def predict():
     input_query=np.array([url])
     #print(input_query)
 
-
-    # Reading data from csv file
-    data = pd.read_csv('urldata.csv')
-    data.head()
-
-    # Labels
-    y = data["label"]
-
-    # Features
-    url_list = data["url"]
-
-
-    # Using Tokenizer
-    vectorizer = TfidfVectorizer()
-
-    # Store vectors into X variable as Our XFeatures
-    X = vectorizer.fit_transform(url_list)
-
-
-
-    # Split into training and testing dataset 80:20 ratio
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-    # Model Building using logistic regression
-    logit = LogisticRegression()
-    logit.fit(X_train, y_train)
-
-    
-
     #vectorizer = TfidfVectorizer()
     input_query = vectorizer.transform(input_query)
     result = logit.predict(input_query)[0]
     #print(result)
     return jsonify({'output':result})
-
 
 
 if __name__ == "__main__":
