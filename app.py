f44a3791db99f5213958d59b569863bd222dca83
Mohammed Misbahuddin <misbahuddinsaudagar@gmail.com>
2023-07-31 08:51:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,76 @@
+
 from flask import Flask, request, jsonify
-import pickle
-from collections import OrderedDict
+# import pickle
+import numpy as np
 
-app = Flask(__name__)
+import pandas as pd
+import numpy as np
+# import random
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-def make_tokens(url):
-    tokens_by_slash = url.split('/')  # make tokens after splitting by slash
-    total_tokens = []
-    for token in tokens_by_slash:
-        tokens = token.split('-')  # make tokens after splitting by dash
-        for sub_token in tokens:
-            tkns_by_dot = sub_token.split('.')  # make tokens after splitting by dot
-            total_tokens.append(sub_token)  # add token to the list
-            total_tokens.extend(tkns_by_dot)  # add dot tokens to the list
-    total_tokens = list(OrderedDict.fromkeys(total_tokens))  # remove redundant tokens while preserving order
-    total_tokens = [token for token in total_tokens if token != 'com']
-    return total_tokens
 
-@app.route('/')
-def home():
-    return "Hello world, this is a test"
 
-@app.route('/predict', methods=['POST'])
+# Accuracy of Our Model
+#print("Accuracy of our model is: ",logit.score(X_test, y_test))
+
+# X_predict = ["google.com/search=jcharistech",
+# "google.com/search=faizanahmad",
+# "pakistanifacebookforever.com/getpassword.php/", 
+# "www.radsport-voggel.de/wp-admin/includes/log.exe", 
+# "ahrenhei.without-transfer.ru/nethost.exe ",
+# "www.itidea.it/centroesteticosothys/img/_notes/gum.exe"]
+
+# X_predict = vectorizer.transform(X_predict)
+# New_predict = logit.predict(X_predict)
+# print(New_predict)
+
+app=Flask(__name__)
+
+@app.route('/predict',methods=['POST'])
 def predict():
-    data = request.json
-    if not data or 'url' not in data:
-        return jsonify({'error': 'Invalid data format. Make sure to provide JSON data with a "url" field.'}), 400
+    url=request.form.get('url')
+    #result={'url':url}
+    input_query=np.array([url])
+    #print(input_query)
 
-    url = data['url']
+    # Reading data from csv file
+    data = pd.read_csv('urldata.csv')
+    data.head()
 
-    # Read the ML model
-    try:
-        with open("malicious_url_model.pkl", "rb") as file:
-            model, vectorizer = pickle.load(file)
-    except FileNotFoundError:
-        return jsonify({'error': 'Model not found. Make sure the model file "malicious_url_model.pkl" is available.'}), 500
-    except Exception as e:
-        return jsonify({'error': f'Error loading the model: {str(e)}'}), 500
 
-    # Vectorize the new URL using the loaded vectorizer
-    try:
-        X_predict = vectorizer.transform([url])
-    except Exception as e:
-        return jsonify({'error': f'Error vectorizing the URL: {str(e)}'}), 500
+    # Labels
+    y = data["label"]
 
-    # Make predictions using the loaded model
-    try:
-        result = model.predict(X_predict)[0]
-    except Exception as e:
-        return jsonify({'error': f'Error making predictions: {str(e)}'}), 500
+    # Features
+    url_list = data["url"]
 
-    return jsonify({'output': result})
+
+    # Using Tokenizer
+    vectorizer = TfidfVectorizer()
+
+    # Store vectors into X variable as Our XFeatures
+    X = vectorizer.fit_transform(url_list)
+
+
+
+    # Split into training and testing dataset 80:20 ratio
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+    # Model Building using logistic regression
+    logit = LogisticRegression()
+    logit.fit(X_train, y_train)
+
+    #vectorizer = TfidfVectorizer()
+    input_query = vectorizer.transform(input_query)
+    result = logit.predict(input_query)[0]
+    #print(result)
+    return jsonify({'output':result})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
