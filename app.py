fb08d727f5c9ea099d79d294da47dedd74bf2594
Mohammed Misbahuddin <misbahuddinsaudagar@gmail.com>
2023-07-31 08:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,37 +3,8 @@ import pickle
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from utils import makeTokens
 
-from collections import OrderedDict
-'''
-# tokens ending with remove
-def makeTokens(f):
-    tkns_BySlash = str(f).split('/')  # make tokens after splitting by slash
-    total_Tokens = []
-    for i in tkns_BySlash:
-        tokens = str(i).split('-')  # make tokens after splitting by dash
-        for token in tokens:
-            tkns_ByDot = token.split('.')  # make tokens after splitting by dot
-            total_Tokens.append(token)  # add token to the list
-            total_Tokens.extend(tkns_ByDot)  # add dot tokens to the list
-    total_Tokens = list(OrderedDict.fromkeys(total_Tokens))  # remove redundant tokens while preserving order
-    total_Tokens = [token for token in total_Tokens if not any(token.endswith(ext) for ext in ["com"])]
-    return total_Tokens
-'''
-def makeTokens(f):
-    tkns_BySlash = str(f).split('/')  # make tokens after splitting by slash
-    total_Tokens = []
-    for i in tkns_BySlash:
-        tokens = str(i).split('-')  # make tokens after splitting by dash
-        for token in tokens:
-            tkns_ByDot = token.split('.')  # make tokens after splitting by dot
-            total_Tokens.append(token)  # add token to the list
-            total_Tokens.extend(tkns_ByDot)  # add dot tokens to the list
-    total_Tokens = list(OrderedDict.fromkeys(total_Tokens))  # remove redundant tokens while preserving order
-    for itm in ['com']:
-        if itm in total_Tokens:
-            total_Tokens.remove(itm)
-    return total_Tokens
 
 app=Flask(__name__)
 
