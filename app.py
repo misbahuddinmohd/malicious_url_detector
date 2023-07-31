506e61aa750cdd7f7da83bfd08f51c0c56e628ae
Mohammed Misbahuddin <misbahuddinsaudagar@gmail.com>
2023-07-31 07:40:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,39 @@
 from flask import Flask, request, jsonify
-#import pickle
-import numpy as np
+import pickle
 import pandas as pd
-#import random
-
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 
-
-# Reading data from csv file
-data = pd.read_csv('urldata.csv')
-data.head()
-
-# Labels
-y = data["label"]
-
-# Features
-url_list = data["url"]
-
-
-# Using Tokenizer
-vectorizer = TfidfVectorizer()
-
-# Store vectors into X variable as Our XFeatures
-X = vectorizer.fit_transform(url_list)
-
-
-
-# Split into training and testing dataset 80:20 ratio
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-# Model Building using logistic regression
-logit = LogisticRegression()
-logit.fit(X_train, y_train)
-
-# url="google.com/search=faizanahmad"
-# input_query=np.array([url])
-# print(input_query)
-
-# input_query = vectorizer.transform(input_query)
-# result = logit.predict(input_query)[0]
-# print(result)
-
+from collections import OrderedDict
+'''
+# tokens ending with remove
+def makeTokens(f):
+    tkns_BySlash = str(f).split('/')  # make tokens after splitting by slash
+    total_Tokens = []
+    for i in tkns_BySlash:
+        tokens = str(i).split('-')  # make tokens after splitting by dash
+        for token in tokens:
+            tkns_ByDot = token.split('.')  # make tokens after splitting by dot
+            total_Tokens.append(token)  # add token to the list
+            total_Tokens.extend(tkns_ByDot)  # add dot tokens to the list
+    total_Tokens = list(OrderedDict.fromkeys(total_Tokens))  # remove redundant tokens while preserving order
+    total_Tokens = [token for token in total_Tokens if not any(token.endswith(ext) for ext in ["com"])]
+    return total_Tokens
+'''
+def makeTokens(f):
+    tkns_BySlash = str(f).split('/')  # make tokens after splitting by slash
+    total_Tokens = []
+    for i in tkns_BySlash:
+        tokens = str(i).split('-')  # make tokens after splitting by dash
+        for token in tokens:
+            tkns_ByDot = token.split('.')  # make tokens after splitting by dot
+            total_Tokens.append(token)  # add token to the list
+            total_Tokens.extend(tkns_ByDot)  # add dot tokens to the list
+    total_Tokens = list(OrderedDict.fromkeys(total_Tokens))  # remove redundant tokens while preserving order
+    for itm in ['com']:
+        if itm in total_Tokens:
+            total_Tokens.remove(itm)
+    return total_Tokens
 
 app=Flask(__name__)
 
@@ -57,14 +46,27 @@ def predict():
     url=request.form.get('url')
     #result={'url':url}
     input_query=np.array([url])
-    #print(input_query)
+    print(input_query)
 
-    #vectorizer = TfidfVectorizer()
-    input_query = vectorizer.transform(input_query)
-    result = logit.predict(input_query)[0]
-    #print(result)
+    # read the ML model
+    with open("malicious_url_model.pkl", "rb") as file:
+        model, vectorizer = pickle.load(file)
+
+    #X_predict = input_query
+    # Re-create the vectorizer object with the same tokenizer function
+    vectorizer.tokenizer = makeTokens
+
+    # Vectorize the new URLs using the loaded vectorizer
+    X_predict = vectorizer.transform(input_query)
+
+    # Make predictions using the loaded model
+    result = model.predict(X_predict)[0]
+    print(result)
+
     return jsonify({'output':result})
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
